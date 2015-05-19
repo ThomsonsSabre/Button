3bf5943e992563eda2c2fdc3801c7b7e76733c89
lsbroad <luke.broadberry@thomsons.com>
2015-05-19 14:06:15 +0100

    initial commit to gitHub button repo

---
+++ SabreSwitch/SabreSwitch/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SabreSwitch.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult okGo(string button)
+        {
+            return View("okGoView");
+        }
+
+    }
+}
